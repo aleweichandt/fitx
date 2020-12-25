7ba216f4dd5d2fbda1313d0d5993f9fe423fa156
Alejandro Weichandt <aleweichandt@gmail.com>
2020-12-25 16:21:41 -0300

    add temp font size to diff headline from paragraph

--- src/base-ui/view/Headline.tsx
+++ src/base-ui/view/Headline.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 
 const Headline = styled.Text`
+  font-size: 20;
   padding: ${(props) => props.theme.spacing}px 0px;
 `;
 
--- src/base-ui/view/Paragraph.tsx
+++ src/base-ui/view/Paragraph.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 
 const Paragraph = styled.Text`
+  font-size: 14;
   padding: ${(props) => props.theme.smallSpacing}px 0px;
 `;
 
