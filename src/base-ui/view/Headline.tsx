84bc5dd27a173a4f1083e42d3316297b445cc35f
Alejandro Weichandt <aleweichandt@gmail.com>
2020-12-26 20:52:42 -0300

    fixed bad font size and apply font styling

--- src/base-ui/view/Headline.tsx
+++ src/base-ui/view/Headline.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 
 const Headline = styled.Text`
-  font-size: 20;
+  font-size: 20px;
+  font-weight: bold;
   padding: ${(props) => props.theme.spacing}px 0px;
 `;
 
--- src/base-ui/view/Paragraph.tsx
+++ src/base-ui/view/Paragraph.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 
 const Paragraph = styled.Text`
-  font-size: 14;
+  font-size: 15px;
+  font-weight: 300;
   padding: ${(props) => props.theme.smallSpacing}px 0px;
 `;
 
